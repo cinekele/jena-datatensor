b261e3816a6acdeafbe646fd63230b1b02d713e2
cinekele <cinekele@gmail.com>
2024-03-13 22:21:01 +0100

    adding boolean test

---
+++ src/test/java/pl/edu/pw/mini/jena/datatensor/datatypes/utils/mapper/BooleanMapperTest.java
@@ -0,0 +1,46 @@
+package pl.edu.pw.mini.jena.datatensor.datatypes.utils.mapper;
+
+import junit.framework.TestCase;
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+
+public class BooleanMapperTest extends TestCase {
+
+    public void testMapJSONtoINDArray() {
+        String json = "{\"shape\":[2,2],\"data\":[true,false,true,false]}";
+        INDArray array = BooleanMapper.mapJSONtoINDArray(json);
+        INDArray expected = Nd4j.create(
+                new boolean[]{true, false, true, false},
+                new long[]{2, 2}, DataType.BOOL);
+        assertEquals(expected, array);
+    }
+
+
+    public void testMapJSONtoINDArrayWithInvalidValue() {
+        String json = "{\"shape\":[2,2],\"data\":[1,false,true,false]}";
+        try {
+            BooleanMapper.mapJSONtoINDArray(json);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid JSON format", e.getMessage().substring(0, 19));
+        }
+    }
+
+    public void testMapJSONtoINDArrayWithInvalidJSON() {
+        String json = "";
+        try {
+            BooleanMapper.mapJSONtoINDArray(json);
+            fail("Expected an IllegalArgumentException to be thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid JSON format", e.getMessage().substring(0, 19));
+        }
+    }
+
+    public void testMapINDArrayToJson() {
+        INDArray array = Nd4j.create(new boolean[]{true, false, true, false}, new long[]{2, 2}, DataType.BOOL);
+        String json = BooleanMapper.mapINDArrayToJson(array);
+        String expected = "{\"shape\":[2,2],\"data\":[true,false,true,false]}";
+        assertEquals(expected, json);
+    }
+}
