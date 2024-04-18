a52c1c27d12bcc84fbe03b63786cf253eb24fe09
cinekele <cinekele@gmail.com>
2024-04-18 20:58:53 +0200

    clean DT

--- src/main/java/pl/edu/pw/mini/jena/datatensor/vocabulary/FunctionsDT.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/vocabulary/FunctionsDT.java
@@ -1,9 +1,6 @@
 package pl.edu.pw.mini.jena.datatensor.vocabulary;
 
 public class FunctionsDT {
-
-    public static final String NS = DataTensor.getURI();
-
     public static final String COSINE_SIMILARITY = DataTensor.getURI() + "cosineSimilarity";
     public static final String EUCLIDEAN_DISTANCE = DataTensor.getURI() + "euclideanDistance";
 }
