c996c803c403a5c42cd95c4f1015f09475350796
cinekele <cinekele@gmail.com>
2024-04-20 20:10:19 +0200

    add Divide

---
+++ src/test/java/pl/edu/pw/mini/jena/datatensor/functions/operators/DivideTest.java
@@ -0,0 +1,54 @@
+package pl.edu.pw.mini.jena.datatensor.functions.operators;
+
+import junit.framework.TestCase;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.nd4j.linalg.api.buffer.DataType;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import pl.edu.pw.mini.jena.datatensor.datatypes.implementations.NumericDataTensor;
+
+public class DivideTest extends TestCase {
+    public void testExec1() {
+        Divide divide = new Divide();
+        NodeValue nodeValue1 = NodeValue.makeNode("{\"type\":\"double\",\"shape\":[2],\"data\":[4.0, 8.0]}", NumericDataTensor.INSTANCE);
+        NodeValue nodeValue2 = NodeValue.makeNode("{\"type\":\"double\",\"shape\":[2],\"data\":[2.0, 2.0]}", NumericDataTensor.INSTANCE);
+        NodeValue resultNode = divide.exec(nodeValue1, nodeValue2);
+        INDArray result = (INDArray) resultNode.getNode().getLiteralValue();
+        INDArray expected = Nd4j.create(new double[]{2.0, 4.0});
+        boolean compare = result.equalsWithEps(expected, 0.0001);
+        assertTrue(compare);
+    }
+
+    public void testExec2() {
+        Divide divide = new Divide();
+        NodeValue nodeValue1 = NodeValue.makeNode("{\"type\":\"float\",\"shape\":[2],\"data\":[1.0, 2.0]}", NumericDataTensor.INSTANCE);
+        NodeValue nodeValue2 = NodeValue.makeNode("{\"type\":\"int\",\"shape\":[2],\"data\":[3, 4]}", NumericDataTensor.INSTANCE);
+        NodeValue resultNode = divide.exec(nodeValue1, nodeValue2);
+        INDArray result = (INDArray) resultNode.getNode().getLiteralValue();
+        INDArray expected = Nd4j.create(new float[]{1.0f / 3.0f, 0.5f});
+        boolean compare = result.equalsWithEps(expected, 0.0001f);
+        assertTrue(compare);
+    }
+
+    public void testExec3() {
+        Divide divide = new Divide();
+        NodeValue nodeValue1 = NodeValue.makeNode("{\"type\":\"short\",\"shape\":[2],\"data\":[1, 2]}", NumericDataTensor.INSTANCE);
+        NodeValue nodeValue2 = NodeValue.makeNode("{\"type\":\"double\",\"shape\":[2, 2],\"data\":[1.0, 3.0, 4.0, 5.0]}", NumericDataTensor.INSTANCE);
+        NodeValue resultNode = divide.exec(nodeValue1, nodeValue2);
+        INDArray result = (INDArray) resultNode.getNode().getLiteralValue();
+        INDArray expected = Nd4j.create(new double[]{1.0, 2.0 / 3.0, 0.25, 0.4}, new int[]{2, 2});
+        boolean compare = result.equalsWithEps(expected, 0.0001);
+        assertTrue(compare);
+    }
+
+    public void testExec4() {
+        Divide divide = new Divide();
+        NodeValue nodeValue1 = NodeValue.makeNode("{\"type\":\"int\",\"shape\":[2],\"data\":[1, 2]}", NumericDataTensor.INSTANCE);
+        NodeValue nodeValue2 = NodeValue.makeNode("{\"type\":\"short\",\"shape\":[2],\"data\":[1, 2]}", NumericDataTensor.INSTANCE);
+        NodeValue resultNode = divide.exec(nodeValue1, nodeValue2);
+        INDArray result = (INDArray) resultNode.getNode().getLiteralValue();
+        INDArray expected = Nd4j.create(new double[]{1.0, 1.0}, new int[]{2}).castTo(DataType.INT32);
+        boolean compare = result.equalsWithEps(expected, 0.0001);
+        assertTrue(compare);
+    }
+}
