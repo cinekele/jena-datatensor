c1a55f2fc4a7ad104e0a31067b39b1ff126345dd
cinekele <cinekele@gmail.com>
2024-04-29 17:17:32 +0200

    fix transformers

--- src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/PolyTransform.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/PolyTransform.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.mini.jena.datatensor.functions.transfomers;
 
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.ops.transforms.Transforms;
 import pl.edu.pw.mini.jena.datatensor.functions.NumericDTNumericFunctionBase;
@@ -11,6 +12,7 @@ public class PolyTransform extends NumericDTNumericFunctionBase {
     }
 
     public INDArray calc(Double v1, INDArray v2) {
-        return Transforms.pow(v2, v1);
+        INDArray casted = v2.dataType().isFPType() ? v2 : v2.castTo(DataType.DOUBLE);
+        return Transforms.pow(casted, v1);
     }
 }
--- src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/ScaleTransform.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/ScaleTransform.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.mini.jena.datatensor.functions.transfomers;
 
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import pl.edu.pw.mini.jena.datatensor.functions.NumericDTNumericFunctionBase;
 
@@ -7,6 +8,7 @@ public class ScaleTransform extends NumericDTNumericFunctionBase {
 
     @Override
     public INDArray calc(Double v1, INDArray v2) {
-        return v2.mul(v1);
+        INDArray casted = v2.dataType().isFPType() ? v2 : v2.castTo(DataType.DOUBLE);
+        return casted.mul(v1);
     }
 }
