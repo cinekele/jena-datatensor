14c8635091298ac199c50822bcd61a7c8758ee86
cinekele <cinekele@gmail.com>
2024-04-29 17:17:52 +0200

    add tests to suite

--- src/test/java/pl/edu/pw/mini/jena/datatensor/functions/TSFunctions.java
+++ src/test/java/pl/edu/pw/mini/jena/datatensor/functions/TSFunctions.java
@@ -3,6 +3,8 @@ package pl.edu.pw.mini.jena.datatensor.functions;
 import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import pl.edu.pw.mini.jena.datatensor.functions.concatenators.TSConcatenators;
+import pl.edu.pw.mini.jena.datatensor.functions.indexers.GetSubDTTest;
 import pl.edu.pw.mini.jena.datatensor.functions.operators.TSOperators;
 import pl.edu.pw.mini.jena.datatensor.functions.similarities.TSSimilarities;
 import pl.edu.pw.mini.jena.datatensor.functions.transfomers.TSTransformers;
@@ -12,7 +14,9 @@ import pl.edu.pw.mini.jena.datatensor.functions.transfomers.TSTransformers;
         TSOperators.class,
         TSSimilarities.class,
         TSTransformers.class,
-        AggregatesTest.class
+        TSConcatenators.class,
+        AggregatesTest.class,
+        GetSubDTTest.class,
 })
 public class TSFunctions extends TestSuite {
 }
