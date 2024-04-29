8e98f44fae0ec5993865682463ceacae2553e93f
cinekele <cinekele@gmail.com>
2024-04-29 17:11:49 +0200

    fix logp transform

--- src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/LogPTransform.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/functions/transfomers/LogPTransform.java
@@ -1,5 +1,6 @@
 package pl.edu.pw.mini.jena.datatensor.functions.transfomers;
 
+import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.ops.transforms.Transforms;
 import pl.edu.pw.mini.jena.datatensor.functions.NumericDTNumericFunctionBase;
@@ -8,6 +9,7 @@ public class LogPTransform extends NumericDTNumericFunctionBase {
 
     @Override
     public INDArray calc(Double v1, INDArray v2) {
-        return Transforms.log(v2, v1);
+        INDArray casted = v2.dataType().isIntType() ? v2.castTo(DataType.DOUBLE) : v2;
+        return Transforms.log(casted, v1);
     }
 }
