477cd0bb5412d3af66848ead96f88fc8ed8ddc02
cinekele <cinekele@gmail.com>
2024-05-03 16:41:29 +0200

    fix VStack And Hstack

--- src/main/java/pl/edu/pw/mini/jena/datatensor/functions/concatenators/VStack.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/functions/concatenators/VStack.java
@@ -8,14 +8,18 @@ import org.nd4j.linalg.factory.Nd4j;
 import pl.edu.pw.mini.jena.datatensor.datatypes.BaseDataTensor;
 import pl.edu.pw.mini.jena.datatensor.datatypes.implementations.BooleanDataTensor;
 import pl.edu.pw.mini.jena.datatensor.datatypes.implementations.NumericDataTensor;
+import pl.edu.pw.mini.jena.datatensor.datatypes.utils.ND4JUtils;
 import pl.edu.pw.mini.jena.datatensor.functions.GenericDT2FunctionBase;
 
 public class VStack extends GenericDT2FunctionBase {
 
     @Override
     public NodeValue calc(INDArray v1, INDArray v2) {
-        INDArray result = Nd4j.vstack(v1, v2);
-        BaseDataTensor dt = v1.dataType().equals(DataType.BOOL) ? BooleanDataTensor.INSTANCE : NumericDataTensor.INSTANCE;
+        DataType dataType = ND4JUtils.getSupportedOperationType(v1, v2);
+        INDArray v1casted = v1.dataType().equals(dataType) ? v1 : v1.castTo(dataType);
+        INDArray v2casted = v2.dataType().equals(dataType) ? v2 : v2.castTo(dataType);
+        INDArray result = Nd4j.vstack(v1casted, v2casted);
+        BaseDataTensor dt = v1.dataType().equals(DataType.BOOL) && v2.dataType().equals(DataType.BOOL) ? BooleanDataTensor.INSTANCE : NumericDataTensor.INSTANCE;
         return NodeValue.makeNode(NodeFactory.createLiteralByValue(result, dt));
     }
 }
