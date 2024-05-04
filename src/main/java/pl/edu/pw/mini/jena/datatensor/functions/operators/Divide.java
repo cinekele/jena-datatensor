fbc8e8c2f1d9d386b1c1fe34231e56f54a173c76
cinekele <cinekele@gmail.com>
2024-05-04 13:12:33 +0200

    fix Divide

--- src/main/java/pl/edu/pw/mini/jena/datatensor/functions/operators/Divide.java
+++ src/main/java/pl/edu/pw/mini/jena/datatensor/functions/operators/Divide.java
@@ -1,6 +1,7 @@
 package pl.edu.pw.mini.jena.datatensor.functions.operators;
 
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
@@ -15,6 +16,9 @@ public class Divide extends NumericDT2FunctionBase {
         DataType dataType = ND4JUtils.getSupportedOperationType(v1, v2);
         v1 = v1.dataType().equals(dataType) ? v1 : v1.castTo(dataType);
         v2 = v2.dataType().equals(dataType) ? v2 : v2.castTo(dataType);
+        if (v2.eq(0).any()) {
+            throw new ExprEvalException("Division by zero");
+        }
         INDArray result = v1.div(v2);
         return NodeValue.makeNode(NodeFactory.createLiteralByValue(result, NumericDataTensor.INSTANCE));
     }
