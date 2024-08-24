9e4bb39f3432ac6bce82f93524295b7c1914af3b
cinekele <cinekele@gmail.com>
2024-08-24 17:16:06 +0200

    add test suite for reductors

--- src/test/java/pl/edu/pw/mini/jena/datatensor/functions/TSFunctions.java
+++ src/test/java/pl/edu/pw/mini/jena/datatensor/functions/TSFunctions.java
@@ -1,21 +1,22 @@
 package pl.edu.pw.mini.jena.datatensor.functions;
 
-import junit.framework.TestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import pl.edu.pw.mini.jena.datatensor.functions.concatenators.TSConcatenators;
 import pl.edu.pw.mini.jena.datatensor.functions.indexers.GetSubDTTest;
 import pl.edu.pw.mini.jena.datatensor.functions.operators.TSOperators;
+import pl.edu.pw.mini.jena.datatensor.functions.reductors.TSReductors;
 import pl.edu.pw.mini.jena.datatensor.functions.similarities.TSSimilarities;
 import pl.edu.pw.mini.jena.datatensor.functions.transfomers.TSTransformers;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        TSOperators.class,
-        TSSimilarities.class,
-        TSTransformers.class,
         TSConcatenators.class,
         GetSubDTTest.class,
+        TSOperators.class,
+        TSReductors.class,
+        TSTransformers.class,
+        TSSimilarities.class,
 })
-public class TSFunctions extends TestSuite {
+public class TSFunctions {
 }
